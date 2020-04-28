6cb033778c91b8f9618fcca9477793c806604d9c
Firat Anik <firat.anik.fa@gmail.com>
2020-04-28 08:58:48 +0000

    DownChevron System Completed
    DownChevron System Completed
    DownChevron AnimationsCompleted

---
+++ Assets/Scripts/Controller/DownChevronController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DownChevronController : MonoBehaviour
+{
+    public GameObject DownChevronPrefab;
+    public int AmountOfDownChevron;
+    public float AnimationRepSecond;
+    [Space]
+    public Transform FirstSpawnPoint;
+    public float ZAxisSpace;
+    [Tooltip("If this is not checked on that means those will be spawn to backwards")]
+    public bool KeepSpawnForward;
+
+    private int currentIndex;
+    private List<MeshRenderer> DownChevrons = new List<MeshRenderer>();
+    private List<Material> CloneMaterials = new List<Material>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = FirstSpawnPoint.position;
+
+        for (int i = 0; i < AmountOfDownChevron; i++)
+        {
+            GameObject go = Instantiate(DownChevronPrefab);
+            go.transform.position = pos;
+            if (KeepSpawnForward)
+                pos.z += ZAxisSpace;
+            else
+                pos.z -= ZAxisSpace;
+
+            DownChevrons.Add(go.GetComponent<MeshRenderer>());
+            CloneMaterials.Add(go.GetComponent<MeshRenderer>().material);
+            DownChevrons.Last().material = CloneMaterials.Last();
+        }
+
+        CloneMaterials.Reverse();
+
+        InvokeRepeating("InvokeLightUp", AnimationRepSecond, 1);
+    }
+
+    private void InvokeLightUp()
+    {
+        if (++currentIndex>DownChevrons.Count-1)
+            currentIndex = 0;
+
+        for (int i = 0; i < CloneMaterials.Count; i++)
+        {
+            CloneMaterials[i].color = Color.white;
+        }
+
+        CloneMaterials[currentIndex].color = Color.red;
+
+    }
+}
